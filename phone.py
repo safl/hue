6b3127e5f48e823dcea7ff60ce751bdd3d701dfb
Simon A. F. Lund <safl@safl.dk>
2015-10-19 01:41:28 +0200

    Fixed another typo...

--- phone.py
+++ phone.py
@@ -34,7 +34,7 @@ def main():
                                     # Register callbacks
     l.register_keyPress(keys["0"], lambda event: b.toggle())
   
-    for n in xrange(1, 6+1)         # Map 1-6 keypad to toggling lights 1-6
+    for n in xrange(1, 6+1):        # Map 1-6 keypad to toggling lights 1-6
         l.register_keyPress(keys["%d" % n], lambda event: b.toggle(bulb2hue(n)))
 
     #l.register_keyPress(keys["6"], lambda event: b.set_state(states["Energize"]))
