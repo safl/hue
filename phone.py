dd5e0a9be1102efdd63ae56fbcbe30a466399942
Simon A. F. Lund <safl@safl.dk>
2015-10-19 02:11:43 +0200

    ...

--- phone.py
+++ phone.py
@@ -34,8 +34,12 @@ def main():
                                     # Register callbacks
     l.register_keyPress(keys["0"], lambda event: b.toggle())
   
-    for n in xrange(1, 6+1):        # Map 1-6 keypad to toggling lights 1-6
-        l.register_keyPress(keys["%d" % n], lambda event: b.toggle(bulb2hue[n]))
+    l.register_keyPress(keys["1"], lambda event: b.toggle(bulb2hue[1]))
+    l.register_keyPress(keys["2"], lambda event: b.toggle(bulb2hue[2]))
+    l.register_keyPress(keys["3"], lambda event: b.toggle(bulb2hue[3]))
+    l.register_keyPress(keys["4"], lambda event: b.toggle(bulb2hue[4]))
+    l.register_keyPress(keys["5"], lambda event: b.toggle(bulb2hue[5]))
+    l.register_keyPress(keys["6"], lambda event: b.toggle(bulb2hue[6]))
 
     #l.register_keyPress(keys["6"], lambda event: b.set_state(states["Energize"]))
     l.register_keyPress(keys["7"], lambda event: b.set_state(states["Concentrate"]))
